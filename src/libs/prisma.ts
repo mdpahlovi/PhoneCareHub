d17c450f7cb6058dbc9d267fb919867589005147
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2024-03-22 12:09:50 +0600

    [Fixed] Build Error

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -1,15 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-    return new PrismaClient();
-};
-
-declare global {
-    var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
-}
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma = new PrismaClient({ errorFormat: "minimal" });
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
