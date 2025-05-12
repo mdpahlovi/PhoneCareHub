cf2e293c054b37f1dbf8c80fdd2872c06e369548
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2025-05-12 23:50:50 +0600

    [Fixed] Auth Layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import LayoutTitle from "@/components/Auth/LayoutTitle";
 import ThemedLogo from "@/components/Common/ThemedLogo";
 import { Copyright } from "@/exports/mui";
-import { Divider, Grid } from "@mui/material";
+import { Box, Divider, Stack } from "@mui/material";
 import Image from "next/image";
 
 export default function AuthLayout({ children }: React.PropsWithChildren) {
@@ -9,17 +9,26 @@ export default function AuthLayout({ children }: React.PropsWithChildren) {
     const mdHidden = { xs: "none", md: "flex" };
 
     return (
-        <Grid container alignItems="center" flexWrap="nowrap" gap={gap} px={gap} sx={{ minHeight: "100vh" }}>
-            <Grid size={{ md: 5, lg: 6 }} display={mdHidden} direction="column" alignItems="center" gap={4}>
+        <Box
+            sx={{
+                px: gap,
+                height: "100vh",
+                display: "grid",
+                alignItems: "center",
+                gridTemplateColumns: { xs: "1fr", md: "1fr 1px 1fr" },
+                gap,
+            }}
+        >
+            <Stack display={mdHidden} gap={4}>
                 <LayoutTitle />
                 <Image src="/images/auth.png" alt="" width={480} height={360} />
-            </Grid>
-            <Divider orientation="vertical" flexItem sx={{ display: mdHidden }} />
-            <Grid size={{ xs: 12, md: 7, lg: 6 }} my={4} display="flex" direction="column" gap={4}>
+            </Stack>
+            <Divider orientation="vertical" sx={{ display: mdHidden }} />
+            <Stack gap={4}>
                 <ThemedLogo />
                 {children}
                 <Copyright />
-            </Grid>
-        </Grid>
+            </Stack>
+        </Box>
     );
 }
